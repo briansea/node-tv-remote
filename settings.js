f809bf7369cba2fc7d1df4cf936afad5bb6beba1
root <root@livingroom-pi.(none)>
2013-08-12 18:56:37 +0000

    + Add comma

--- settings.js
+++ settings.js
@@ -3,6 +3,6 @@ var settings = {
 	httpPort: 80,
 	socketIoPort: 1337,
 	mediaBasePath: '/media',
-	omxplayerArgs: ["-s", "-o", "hdmi"]
+	omxplayerArgs: ["-s", "-o", "hdmi"],
 	otherTvServers: ["livingroom-pi.local"],
 };
